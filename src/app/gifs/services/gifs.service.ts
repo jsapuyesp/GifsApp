20d54cbfb656965fc1f56d14ffa86d2d8c82deb3
jsapuyesp <jsapuyesp@gmail.com>
2022-08-29 16:17:45 -0500

    Some details Fixed!

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -40,7 +40,7 @@ export class GifsService {
 
     const params = new HttpParams()
       .set('api_key', this._apiKey)
-      .set('limit', '10')
+      .set('limit', '25')
       .set('q', query);
 
 
