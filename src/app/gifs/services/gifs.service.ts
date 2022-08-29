851ea98d68ce8e200cf2cf30784e0b7f721566a4
jsapuyesp <jsapuyesp@gmail.com>
2022-08-29 11:25:29 -0500

    API Connected

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+
+  private _apiKey: string = 'RLmbCycQEUyABOdG7b2SBLkBfV2HC9Zc'
+
   private _history: string[] = [];
+
+  // TODO: Change this type
+  public results: any[] = [];
 
   get history() {
     return [...this._history]
   }
+
+  constructor(private http: HttpClient) { }
 
   searchGifs(query: string) {
 
@@ -18,6 +27,14 @@ export class GifsService {
       this._history.unshift(query);
       this._history = this._history.splice(0, 10);
     }
+
+    // Http petitioner
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this._apiKey}&q=${query}&limit=10`)
+      .subscribe((response: any) => {
+        console.log(response.data);
+        this.results = response.data;
+      })
+
   }
 
 }
