c47ab7c3f4d4aa5c1aff3676bd946d30f2712806
jsapuyesp <jsapuyesp@gmail.com>
2022-08-28 21:43:49 -0500

    Aplicación-Avanzada

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+  private _history: string[] = [];
+
+  get history() {
+    return [...this._history]
+  }
+
+  searchGifs(query: string) {
+
+    query = query.trim().toLocaleLowerCase()
+
+    if (!this._history.includes(query)) {
+      this._history.unshift(query);
+      this._history = this._history.splice(0, 10);
+    }
+  }
+
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+
+  constructor(private gifService: GifsService) { }
+
+  get history() {
+    return this.gifService.history;
+  }
+}
